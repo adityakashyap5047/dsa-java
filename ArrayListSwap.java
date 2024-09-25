eabe74f45cf300f04c946e4fe912c45b53d41d05
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-26 03:29:42 +0530

    Refactor ArrayListSwap to use a swap method for efficient element swapping

--- ArrayListSwap.java
+++ ArrayListSwap.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 
 public class ArrayListSwap {
+
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(2);
@@ -8,11 +14,8 @@ public class ArrayListSwap {
         list.add(9);
         list.add(3);
         list.add(6);
-        
-        int temp = list.get(1);
-        list.set(1, list.get(3));
-        list.set(3, temp);
 
+        swap(list, 1, 3);
         System.out.println(list);
     }
 }
