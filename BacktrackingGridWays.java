6c77ee58dc362ad98ed80256a26a5dee18233a24
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-26 01:46:28 +0530

    completing the backtracking grid ways

---
+++ BacktrackingGridWays.java
@@ -0,0 +1,28 @@
+public class BacktrackingGridWays {
+
+    /*
+     * Time complexity:
+     * 
+     *          2^(n + m)
+     */
+
+    public static int gridWays(int i, int j, int n, int m) {
+        //base case
+        if(i == n-1 && j == m-1) {          // last cell condition
+            return 1;
+        } else if (i == n || j == m) {      //boundary condition
+            return 0;
+        }
+        int w1 = gridWays(i+1, j, n, m);
+        int w2 = gridWays(i, j+1, n, m);
+        return w1+w2;
+    }
+    
+    public static void main(String[] args) {
+
+        int n = 3, m = 3;
+        System.out.println("The total number of ways is: "+gridWays(0, 0, n, m));
+
+        
+    }
+}
