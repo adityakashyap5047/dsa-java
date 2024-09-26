5619d851f602cc352efe13b96269864b6adddb0e
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-26 07:08:38 +0530

    Refactor ContainerWithMostWater to use a more efficient two-pointer approach for calculating stored water

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -3,12 +3,29 @@ public class ContainerWithMostWater {
 
     public static int storedWater(ArrayList<Integer> height){
         int maxWaterQuatity = 0;
-        //brute force approach
+        //brute force approach       -> O(n^2)
         for(int i = 0; i < height.size(); i++){
             for(int j = i+1; j < height.size(); j++){
                 int waterHeight = Math.min(height.get(i), height.get(j));
                 int waterWidth = j - i;
                 maxWaterQuatity = Math.max(waterHeight * waterWidth, maxWaterQuatity);
+            }
+        }
+        return maxWaterQuatity;
+    }
+
+    public static int storedWaterPointerApproach(ArrayList<Integer> height){
+        int maxWaterQuatity = 0;
+        //Two pointer approach -> O(n)
+        int lp = 0, rp = height.size() - 1;
+        while(lp < rp){
+            int waterHeight = Math.min(height.get(lp), height.get(rp));
+            int waterWidth = rp - lp;
+            maxWaterQuatity = Math.max(waterHeight * waterWidth, maxWaterQuatity);
+            if(height.get(lp) < height.get(rp)){
+                lp++;
+            } else {
+                rp--;
             }
         }
         return maxWaterQuatity;
@@ -26,6 +43,7 @@ public class ContainerWithMostWater {
         height.add(3);
         height.add(7);
         
+        System.out.println("The maximum water stored in the container is: "+storedWaterPointerApproach(height));
         System.out.println("The maximum water stored in the container is: "+storedWater(height));
     }
 }
