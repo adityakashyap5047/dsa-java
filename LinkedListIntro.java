5c6f0a30275333e935d533697d574af5bcf40be7
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-26 23:39:57 +0530

    adding element in the first of ll

--- LinkedListIntro.java
+++ LinkedListIntro.java
@@ -8,8 +8,32 @@ public class LinkedListIntro {
             this.data = data;
             this.next = null;
         }
+
     }
+    
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+
+        //step1 = create new node
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //step2 - newNode = head
+        newNode.next = head;    //link 
+
+        //step3 - head = newNode
+        head = newNode;
+    }
+
     public static void main(String[] args) {
-        
+        LinkedListIntro ll = new LinkedListIntro();
+        ll.addFirst(1);
+        ll.addFirst(4);
     }
 }
