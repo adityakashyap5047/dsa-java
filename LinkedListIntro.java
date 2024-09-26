220733b13707340b79977d1ea2070a153d126457
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-26 23:57:45 +0530

    printing the LL

--- LinkedListIntro.java
+++ LinkedListIntro.java
@@ -1,5 +1,5 @@
 public class LinkedListIntro {
-    
+
     public static class Node {
         int data;
         Node next;
@@ -10,32 +10,32 @@ public class LinkedListIntro {
         }
 
     }
-    
+
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data){
+    public void addFirst(int data) {        // T.C - O(1)
 
-        //step1 = create new node
+        // step1 = create new node
         Node newNode = new Node(data);
 
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
 
-        //step2 - newNode = head
-        newNode.next = head;    //link 
+        // step2 - newNode = head
+        newNode.next = head; // link
 
-        //step3 - head = newNode
+        // step3 - head = newNode
         head = newNode;
     }
 
-    public void addLast(int data){
+    public void addLast(int data) {             // T.C - O(1)
 
         Node newNode = new Node(data);
 
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -44,11 +44,27 @@ public class LinkedListIntro {
         tail = newNode;
     }
 
+    public void printLinkedList(){             // T.C - O(n)
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         LinkedListIntro ll = new LinkedListIntro();
+        ll.printLinkedList();
         ll.addFirst(1);
         ll.addFirst(4);
+        ll.printLinkedList();
         ll.addLast(3);
         ll.addLast(12);
+        ll.printLinkedList();
     }
 }
