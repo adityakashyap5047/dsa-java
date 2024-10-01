b87678889202e7b91248b3b4716b9cd749f53656
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-01 13:56:12 +0530

    circular queue using array

---
+++ QueueArrayCircular.java
@@ -0,0 +1,21 @@
+public class QueueArrayCircular {
+    
+    static class Queue {
+    
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1 && front == -1;
+        }
+    }
+}
