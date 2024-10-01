98548191f9c998db36b9257a7459af6b9436e6d8
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-01 22:23:57 +0530

    circular Queue using array

--- QueueArrayCircular.java
+++ QueueArrayCircular.java
@@ -17,5 +17,61 @@ public class QueueArrayCircular {
         public static boolean isEmpty(){
             return rear == -1 && front == -1;
         }
+
+        public static boolean isFull(){
+            return (rear + 1) % size == front;
+        }
+
+        //add
+        public static void add(int data){               //T.C - O(1)
+            if (isFull()) {
+                System.out.println("Queue is full");
+                return;
+            }
+            //adding 1st element
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        //remove
+        public static int remove(){             //T.C - O(1)
+            if (isEmpty()) {
+                System.out.println("Queue is Empty");
+                return Integer.MIN_VALUE;
+            }
+            int result = arr[front];
+            //last element delete
+            if(rear == front){
+                rear = front = -1;
+            } else{
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        public static int peek() {      // T.C - O(1)
+            if (isEmpty()) {
+                return Integer.MIN_VALUE;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
     }
 }
