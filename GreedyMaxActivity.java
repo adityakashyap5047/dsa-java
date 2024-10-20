1ddf14330fb1bb07d92f1587dc2efe4c29e572ba
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-20 15:10:46 +0530

    starting with greedy algorithm

---
+++ GreedyMaxActivity.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class GreedyMaxActivity{
+
+    public static void main(String[] args) {
+        
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int end[] = {2, 4, 6, 7, 9, 9};
+
+        //end time basis sorted
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        //1st activity ---> it ends earlier from all the activity
+        maxAct = 1;
+        ans.add(0);
+        int lastEnd = end[0];
+
+        
+        /*The error in your code lies in the condition used to select activities. Specifically, the condition if (end[i - 1] <= start[i]) is incorrect. This condition compares the end time of the previous activity in the list with the start time of the current activity, which is not the correct way to determine if the current activity can be selected.
+
+        Instead, you should compare the end time of the last selected activity with the start time of the current activity. To fix this, you need to keep track of the end time of the last selected activity.*/
+
+
+        for(int i = 1; i<end.length; i++){
+            if (lastEnd <= start[i]) {
+                
+                //select the activity
+                maxAct++;
+                ans.add(i);
+                lastEnd = end[i];
+            }
+        }
+
+        System.out.println("max activities is : "+ maxAct);
+        for(int i = 0; i < ans.size(); i++){
+            System.out.print(ans.get(i)+" ");
+        }
+    }
+}
