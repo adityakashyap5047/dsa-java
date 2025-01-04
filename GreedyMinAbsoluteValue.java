cb21d5ae45681a47902668c6a141ff202d5db5a7
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-04 23:23:17 +0530

    finding the sum of minimum absolute value using greedy

---
+++ GreedyMinAbsoluteValue.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class GreedyMinAbsoluteValue {
+    
+    public static void main(String[] args) {
+        
+        int A[] = {1, 2, 3};
+        int B[] = {2, 1, 3};
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for(int i = 0; i < A.length; i++){
+            minDiff += Math.abs(A[i]-B[i]);
+        }    
+
+        System.out.println("The sum of minimum absolute value is = "+minDiff);
+    }
+}
