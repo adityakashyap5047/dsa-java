61d1f843c01ceb2074641abcde9fbd3a3dfac950
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-07 01:04:27 +0530

    print in range in bst

---
+++ BSTPrintInRange.java
@@ -0,0 +1,62 @@
+public class BSTPrintInRange {
+    
+    static class Node {
+    
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val){
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (val < root.data) {
+            //left subtree
+            root.left = insert(root.left, val);
+        } else {
+            //right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void printInRange(Node root, int k1, int k2){
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data > k2) {
+            printInRange(root.left, k1, k2);
+        } else {
+            printInRange(root.right, k1, k2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+
+        Node root = null;
+
+        for(int i = 0; i < values.length; i++){
+            root = insert(root, values[i]);
+        }
+
+        printInRange(root, 4, 12);
+        System.out.println();
+        printInRange(root, 9, 15);
+        System.out.println();
+        printInRange(root, 2, 7);
+
+    }
+}
