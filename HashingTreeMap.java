7ebdd9dba7b159ab8b9819610394fb7375c1b378
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-08 04:48:56 +0530

    tree map in java

---
+++ HashingTreeMap.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class HashingTreeMap {
+    
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+
+        tm.put("India", 100);
+        tm.put("China", 150);
+        tm.put("US", 50);
+        tm.put("Nepal", 5);
+        tm.put("Indonesia", 6);
+
+        System.out.println(tm);
+    }
+}
