8e9fd11f30d0ac94f9db2d1023a9ae330df25af9
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-08 12:38:06 +0530

    finding majority element by hashmap

---
+++ HashingMajorityElement.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashingMajorityElement {
+    
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i], 1);
+            } else {
+                hm.put(nums[i], hm.get(nums[i]) + 1);
+            }
+        }
+
+        Set<Integer> keys = hm.keySet();
+        
+        for (Integer key : keys) {
+            if (hm.get(key) > nums.length/3) {
+                System.out.print(key+" ");
+            }
+        }
+    }
+}
