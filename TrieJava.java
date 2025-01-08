84a547fb82cf33363b8f228715f04ab465bc6e3d
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-08 19:23:37 +0530

    search in Trie

--- TrieJava.java
+++ TrieJava.java
@@ -25,11 +25,26 @@ public class TrieJava{
         curr.eow = true;
     }
 
+    public static boolean search(String key){
+        Node curr = root;
+        for(int level = 0; level < key.length(); level++){
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the", "a", "there", "their", "any", "thee"};
 
         for (int i = 0; i < words.length; i++) {
             insert(words[i]);
         }
+
+        System.out.println(search("the"));
+        System.out.println(search("them"));
     }
 }
