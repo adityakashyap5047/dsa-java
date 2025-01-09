57f283a4b89b4ab486440b7534ef57db3d20b545
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-10 02:13:53 +0530

    finding the minimum spanning tree by krushkal's algorithm

---
+++ GraphsMinimumSpanningTreeByKrushkalAlgorithm.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class GraphsMinimumSpanningTreeByKrushkalAlgorithm {
+    
+    static class Edge implements Comparable<Edge>{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+
+        @Override
+        public int compareTo(Edge e2){
+            return this.wt - e2.wt;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> edges){
+        //edges
+        edges.add(new Edge(0, 1, 10));
+        edges.add(new Edge(0, 2, 15));
+        edges.add(new Edge(0, 3, 30));
+        edges.add(new Edge(1, 3, 40));
+        edges.add(new Edge(2, 3, 50));
+    }
+
+    static int n = 4;               //vertices
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init(){
+        for (int i = 0; i < par.length; i++) {
+            par[i] = i;
+        }
+    }
+
+    public static int find(int x){
+        if (x == par[x]) {
+            return x;
+        }
+        return par[x] = find(par[x]);   //path compression optimization
+    }
+
+    public static void union(int a, int b){
+        int parA = find(a);
+        int parB = find(b);
+
+        if (rank[parA] == rank[parB]) {
+            par[parB] = parA;
+            rank[parA]++;
+        } else if (rank[parA] < rank[parB]) {
+            par[parA] = parB;
+        } else{
+            par[parB] = parA;
+        }
+    }
+
+    public static void krushkalsMST(ArrayList<Edge> edges, int V){
+        init();
+        Collections.sort(edges);
+        int mstCount = 0;
+        int count = 0;
+
+        for (int i = 0; count < V-1; i++) {
+            Edge e = edges.get(i);
+            //e  -> (src, dest, wt)
+            
+            int parA = find(e.src);     //src = A
+            int parB = find(e.dest);    //src = B
+
+            if (parA != parB) {
+                union(e.src, e.dest);
+                mstCount+= e.wt;
+                count++;
+            }
+        }
+
+        System.out.println(mstCount);
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Edge> edges = new ArrayList<>();
+        createGraph(edges);
+        krushkalsMST(edges, V);
+    }
+}
