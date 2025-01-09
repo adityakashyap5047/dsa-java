e8c19c6288350c6c3c6405c73510b487dbc94923
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-09 12:52:51 +0530

    dfs traversal for connected components

--- GraphDFSConnectedComponents.java
+++ GraphDFSConnectedComponents.java
@@ -14,10 +14,41 @@ public class GraphDFSConnectedComponents {
         }
     }
 
+    static void createGraph(ArrayList<Edge> graph[]){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+        
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
     public static void dfs(ArrayList<Edge>[] graph){
         boolean visited[] = new boolean[graph.length];
         for(int i = 0; i < graph.length; i++){
-            dfsUtil(graph, i, visited);
+            if (!visited[i]) {
+                dfsUtil(graph, i, visited);
+            }
         }
     }
 
@@ -32,5 +63,23 @@ public class GraphDFSConnectedComponents {
                 dfsUtil(graph, e.dest, visited);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        /*
+         * 
+         *      1 --- 3   
+         *     /      | \
+         *    0       |  5 --- 6
+         *    \       | /
+         *     2 ---- 4
+         * 
+        */
+
+        int V = 7;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        dfs(graph);
     }
 }
