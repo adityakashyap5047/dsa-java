1bdf8a52d92663a3f09f2d6600950203f1f6c1be
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-11 02:30:22 +0530

    fibonaccci using tabulation (dp)

--- DPFibonacci.java
+++ DPFibonacci.java
@@ -22,11 +22,29 @@ public class DPFibonacci{
         return fibn[n];
     }
 
+    public static int fibTabulation(int n){     // T.C - O(n)
+        int dp[] = new int[n+1];
+
+        //initialize
+        dp[0] = 0;
+        dp[1] = 1;
+
+        //meaning -> fib(n) = dp[n]
+
+        //filling
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println(fib(n));
 
         int fibn[] = new int[n+1];
         System.out.println(dpFib(n, fibn));
+        System.out.println(fibTabulation(n));
     }
 }
