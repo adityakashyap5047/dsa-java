6250dd6fa1460f8f0e2d8f1700d7b73959980b7a
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-11 02:12:32 +0530

    fibonacci using dp

--- DPFibonacci.java
+++ DPFibonacci.java
@@ -1,6 +1,6 @@
 public class DPFibonacci{
 
-    public static int fib(int n){
+    public static int fib(int n){       // T.C - O(2^n)
         if (n == 0 || n == 1) {
             return n;
         }
@@ -8,7 +8,25 @@ public class DPFibonacci{
         return fib(n-1) + fib(n-2);
     }
 
+    public static int dpFib(int n, int fibn[]){     // T.C - O(n)
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (fibn[n] != 0) {
+            return fibn[n];
+        }
+
+        fibn[n] = dpFib(n-1, fibn) + dpFib(n-2, fibn);
+        return fibn[n];
+    }
+
     public static void main(String[] args) {
-        System.out.println(fib(5));
+        int n = 5;
+        System.out.println(fib(n));
+
+        int fibn[] = new int[n+1];
+        System.out.println(dpFib(n, fibn));
     }
 }
