b44c4580b5c194785715fc15b0910e5c44770ed3
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-11 03:28:55 +0530

    climbing stairs with 1 2 3 climb using memoization

--- DPClimbStairs123.java
+++ DPClimbStairs123.java
@@ -10,6 +10,21 @@ public class DPClimbStairs123 {
         return climbingWays(n-1) + climbingWays(n-2) + climbingWays(n-3);
     }
 
+    public static int climbingWaysMem(int n, int dp[]){
+        if (n == 0 || n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = climbingWays(n-1) + climbingWays(n-2) + climbingWays(n-3);
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 4;
 
@@ -17,5 +32,11 @@ public class DPClimbStairs123 {
 
         //recursion
         System.out.println(climbingWays(n));
+
+        // T.C - O(n)
+
+        //memoization
+        int dp[] = new int[n+1];
+        System.out.println(climbingWaysMem(n, dp));
     }
 }
