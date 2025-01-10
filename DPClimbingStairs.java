b1c8dd8cd017345d24adfb4633d21d91a87adf5c
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-11 03:09:36 +0530

    climbing ways using memoization

--- DPClimbingStairs.java
+++ DPClimbingStairs.java
@@ -7,11 +7,24 @@ public class DPClimbingStairs {
         return climbingWays(n-1) + climbingWays(n-2);
     }
 
-    
+    public static int climbingWaysMem(int n, int dp[]){
+        if (n == 0 || n == 1) {
+            return 1;
+        } 
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = climbingWaysMem(n - 1, dp) + climbingWaysMem(n - 2, dp);
+        return dp[n];
+    }
     
     public static void main(String[] args) {
         int n = 5;
         System.out.println(climbingWays(n));
 
+        int dp[] = new int[n+1];
+        System.out.println(climbingWaysMem(n, dp));
     }
 }
