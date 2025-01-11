70f5af6c011d9ed76b3feb95f75cbf438823da90
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-12 05:18:08 +0530

    explanation of the minimum operation for convert a string

--- DPEditDistance.java
+++ DPEditDistance.java
@@ -21,7 +21,7 @@ public class DPEditDistance {
                 if (word1.charAt(i-1) == word2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1];
                 } else{
-                    int ans1 = dp[i][j-1] + 1;  //adding character into word1
+                    int ans1 = dp[i][j-1] + 1;  //adding character into word1 //here when we insert a character into str1, we are trying to match the j-th character of str2 with the next character in str1 therefore the remaining unmatched string is dp[i][j-1]
                     int ans2 = dp[i-1][j] + 1;  //deleting character from word2
                     int ans3 = dp[i-1][j-1] + 1;    //replacing the character from word1 and word2
                     dp[i][j] = Math.min(Math.min(ans1, ans2), ans3);
