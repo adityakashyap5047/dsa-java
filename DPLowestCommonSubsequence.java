943ade1cea28d0c88e9d427b8a7558b21e5220e2
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-12 02:50:28 +0530

    lcs using tabulation

--- DPLowestCommonSubsequence.java
+++ DPLowestCommonSubsequence.java
@@ -37,13 +37,42 @@ public class DPLowestCommonSubsequence {
         return dp[n][m];
     }
 
+    public static int lcsTab(String str1, String str2, int n, int m){
+        int dp[][] = new int[n+1][m+1];
+
+        //initialize
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        //filling
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if (str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     public static void main(String[] args) {
         String str1 = "abcde";
         String str2 = "ace";
         System.out.println(lcs(str1, str2, str1.length(), str2.length()));
 
         //memoization
-        int dp[][] = new int[str1.length()+1][str2.length()+1];
+        int dp[][] = new int[str1.length()+1][str2.length()+1]; // 0 to n -> n+1 numbers
         System.out.println(lcsMem(str1, str2, str1.length(), str2.length(), dp));
+
+        //Tabulation
+        System.out.println(lcsTab(str1, str2, str1.length(), str2.length()));
     }
 }
