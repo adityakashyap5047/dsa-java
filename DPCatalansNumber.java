4492e7f7483137f4c077313a4117b9e218d97c58
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-12 16:08:56 +0530

    catalan number using memoization

--- DPCatalansNumber.java
+++ DPCatalansNumber.java
@@ -1,23 +1,41 @@
 public class DPCatalansNumber {
 
-    public static int catlan(int n){
+    public static int catalan(int n){
         if (n == 0 || n == 1) {
             return 1;
         }
 
         int catN = 0;
         for (int i = 0; i < n; i++) {
-            catN += catlan(i)*catlan(n-1-i);
+            catN += catalan(i)*catalan(n-1-i);
         }
 
         return catN;
     }
 
-    
+    public static int catalan(int n, int dp[]){
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += catalan(i)*catalan(n-1-i);
+        }
+
+        return dp[n] = ans;
+    }
     
     public static void main(String[] args) {
-        System.out.println(catlan(5));
+        System.out.println(catalan(5));
 
-
+        //memoization
+        int n = 5;
+        int dp[] = new int[n+1];
+        System.out.println(catalan(n, dp));
     }
 }
