9d1f8721a2640d2add3bc4ff7b78968f541d7f22
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-13 03:01:05 +0530

    query on the max element segment tree

--- STMaxElementQueries.java
+++ STMaxElementQueries.java
@@ -22,6 +22,26 @@ public class STMaxElementQueries {
         return tree[STIdx];
     }
 
+    public static int getMaxUtil(int STIdx, int si, int sj, int qi, int qj){
+        if (qj <= si || qi >= sj) {  // non overlapping
+            return Integer.MIN_VALUE;
+        } else if (si >= qi && sj <= qj) {  //complete overlap
+            return tree[STIdx];
+        } else {
+            int mid = (si + sj)/2;
+            int leftSubtree = getMaxUtil(2 * STIdx + 1, si, mid, qi, qj);
+            int rightSubtree = getMaxUtil(2 * STIdx + 2, mid+1, sj, qi, qj);
+
+            return Math.max(leftSubtree, rightSubtree);
+        }
+    }
+
+    public static int getMax(int numbers[], int qi, int qj){
+        int n = numbers.length;
+
+        return getMaxUtil(0, 0, n-1, qi, qj);
+    }
+
     public static void main(String[] args) {
         int numbers[] = {6, 8, -1, 2, 17, 1, 3, 2, 4};
         int n = numbers.length;
@@ -34,5 +54,7 @@ public class STMaxElementQueries {
             System.out.print(tree[i]+" ");
         }
         System.out.println();
+
+        System.out.println(getMax(numbers, 0, 3));
     }
 }
