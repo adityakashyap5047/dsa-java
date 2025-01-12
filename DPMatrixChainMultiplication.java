610bc76bc982e24585071362afdac295726b00e2
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-12 23:06:58 +0530

    mcm using memoization

--- DPMatrixChainMultiplication.java
+++ DPMatrixChainMultiplication.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DPMatrixChainMultiplication {
 
     public static int mcm(int arr[], int i, int j){
@@ -18,9 +20,40 @@ public class DPMatrixChainMultiplication {
 
         return minCost;
     }
+
+    public static int mcmMem(int arr[], int i, int j, int dp[][]){
+        if (i == j) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }        
+
+        int minCost = Integer.MAX_VALUE;
+
+        for (int k = i; k < j; k++) {
+            int cost1 = mcmMem(arr, i, k, dp);
+            int cost2 = mcmMem(arr, k+1, j, dp);
+            int cost3 = arr[i-1]*arr[k]*arr[j];
+
+            int finalCost = cost1 + cost2 + cost3;
+            minCost = Math.min(minCost, finalCost);
+        }
+
+        return dp[i][j] = minCost;
+    }
     
     public static void main(String[] args) {
+        //recursion
         int arr[] = {1, 2, 3, 4, 3};
         System.out.println(mcm(arr, 1, arr.length-1));
+
+        //memoization
+        int dp[][] = new int[arr.length][arr.length];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(mcmMem(arr, 1, arr.length-1, dp));
     }
 }
