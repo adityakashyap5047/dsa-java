6a336d520ad2f602827ff60ea4fe04837acf4e8f
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-13 02:15:47 +0530

    calculating the subarray sum with the help of ST

--- STCreation.java
+++ STCreation.java
@@ -6,20 +6,39 @@ public class STCreation {
         tree = new int[4*n];
     }
 
-    public static int buildST(int numbers[], int stIdx, int st, int end){
+    public static int buildST(int numbers[], int STIdx, int st, int end){
         if (st == end) {
-            tree[stIdx] = numbers[st];
+            tree[STIdx] = numbers[st];
             return numbers[st];
         }
 
         int mid = (st + end)/2;
 
-        buildST(numbers, 2 * stIdx + 1, st, mid);  // left subtree -> 2*i + 1
-        buildST(numbers, 2 * stIdx + 2, mid+1, end);  // right subtree -> 2*i + 2
+        buildST(numbers, 2 * STIdx + 1, st, mid);  // left subtree -> 2*i + 1
+        buildST(numbers, 2 * STIdx + 2, mid+1, end);  // right subtree -> 2*i + 2
 
-        tree[stIdx] = tree[2 * stIdx + 1] + tree[2 * stIdx + 2];
+        tree[STIdx] = tree[2 * STIdx + 1] + tree[2 * STIdx + 2];
 
-        return tree[stIdx];
+        return tree[STIdx];
+    }
+
+    public static int getSumUtil(int STIdx, int si, int sj, int qi, int qj){
+        if (qj <= si || qi >= sj) {  // non overlapping
+            return 0;
+        } else if (si >= qi && sj <= qj) {  //complete overlap
+            return tree[STIdx];
+        } else {
+            int mid = (si + sj)/2;
+            int leftSubtree = getSumUtil(2 * STIdx + 1, si, mid, qi, qj);
+            int rightSubtree = getSumUtil(2 * STIdx + 2, mid+1, sj, qi, qj);
+
+            return leftSubtree + rightSubtree;
+        }
+    }
+
+    public static int getSum(int arr[], int qi, int qj){
+        int n = arr.length;
+        return getSumUtil(0, 0, n-1, qi, qj); // si = 0 && sj = n-1 because the root stores the sum of subarray with the length of 0 to n-1
     }
 
     public static void main(String[] args) {
@@ -32,5 +51,8 @@ public class STCreation {
         for (int i = 0; i < tree.length; i++) {
             System.out.print(tree[i]+" ");
         }
+        System.out.println();
+
+        System.out.println(getSum(numbers, 2, 5));
     }
 }
